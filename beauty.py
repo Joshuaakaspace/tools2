df38390fe8da6994392ba1ba270134d16b36a77f
Joshua <75612919+Joshuaakaspace@users.noreply.github.com>
2024-09-22 18:34:10 +0530

    Add files via upload

--- beauty.py
+++ beauty.py
@@ -29,3 +29,42 @@ for div in divs:
 # Output the captured entries
 for entry in entries:
     print(entry)
+
+
+import re
+
+# Example list of strings extracted from the HTML document
+data = [
+    "1 Part 1 of Schedule 1 to the Special Economic Measures (Belarus) Regulations is amended by adding the following in numerical order:",
+    "120 Nikolai Aleksandrovich LUKASHENKO (born on August 31, 2004) (also known as Mikalay Alyaksandravich LUKASHENKA and Kolya LUKASHENKO)",
+    "121 Yuliya Aleksandrovna BLIZNIUK (born on September 23, 1971) (also known as Yuliya Aliaksandrauna BLIZNIUK, Julija Aleksandrovna BLIZNJUK and Julija Aljaksandrauna BLIZNJUK)",
+    "2 Part 1.1 of Schedule 1 to the Regulations is amended by adding the following in numerical order:",
+    "93 Oleg Grigorievich MISHCHENKO (born on August 11, 1968) (also known as Aleh Ryhoravich MISHCHANKA)",
+    "94 Pavel Nikolaevich MURAVEIKO (born on September 26, 1971)",
+    "3 Part 2 of Schedule 1 to the Regulations is amended by adding the following in numerical order:",
+    "71 Design Bureau “Display” JSC",
+    "4 Part 3 of Schedule 1 to the Regulations is amended by adding the following in numerical order:",
+    "2 Open Joint Stock Company \"Minsk Electrotechnical Plant named after V.I. Kozlov\" (also known as OJSC “Minsk Electrotechnical Plant named after V.I. Kozlov” and OJSC “METP NAMED AFTER V.I. KOZLOV”)"
+]
+
+# Regex to match headers indicating the beginning of sections
+header_pattern = re.compile(r'Part \d+(\.\d+)? of Schedule \d+ to the Regulations is amended by adding the following in numerical order:')
+
+# List to hold all the extracted entries
+entries = []
+
+# Variable to keep track of whether we are currently capturing entries
+capturing = False
+
+for line in data:
+    if header_pattern.search(line):
+        capturing = True  # Start capturing entries after this line
+    elif capturing:
+        if re.match(r'^\d+', line.strip()):  # Line starts with one or more digits
+            entries.append(line)
+        else:
+            capturing = False  # Stop capturing if a line doesn't start with a number
+
+# Print the extracted entries
+for entry in entries:
+    print(entry)
