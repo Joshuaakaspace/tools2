c0c29617268bd5eb97a61ad7a3b4fd36ec92be37
Joshua <75612919+Joshuaakaspace@users.noreply.github.com>
2024-10-14 14:52:33 +0530

    Add files via upload

--- NERCHECKS.py
+++ NERCHECKS.py
@@ -13,48 +13,44 @@ def extract_data(input_data):
             'name_ner': item.get('name_ner', '')
         }
         
-        # Extract name
-        name_match = re.search(r"(\w+\s+\w+)\['NAME'\]", item['name_ner'])
-        if name_match:
-            extracted['name'] = name_match.group(1)
+        name_ner = item['name_ner']
         
-        # Extract aka
-        aka_match = re.search(r"Also known as (\w+)\s*\['Also Known As'\]", item['name_ner'])
-        if aka_match:
-            extracted['aka'] = f"Also known as {aka_match.group(1)}"
-        
-        # Extract date of birth
-        dob_match = re.search(r"born on (\d+\s+\w+)\['Date of Birth'\]", item['name_ner'])
-        if dob_match:
-            extracted['date of birth'] = f"born on {dob_match.group(1)}"
+        # Extract information dynamically based on tags
+        tags = re.findall(r'\[\'([^\']+)\'\]', name_ner)
+        for i, tag in enumerate(tags):
+            if i == 0:  # First tag is always NAME
+                extracted['name'] = name_ner[:name_ner.index(f"['{tag}']")].strip()
+            else:
+                start = name_ner.index(f"['{tags[i-1]}']") + len(f"['{tags[i-1]}']")
+                end = name_ner.index(f"['{tag}']")
+                value = name_ner[start:end].strip()
+                
+                if tag == 'ALSO_KNOWN_AS':
+                    extracted['aka'] = value
+                elif tag == 'DATE_OF_BIRTH':
+                    extracted['date of birth'] = value
         
         output.append(extracted)
-    
-    # Add an extra entry with empty 'date of birth'
-    if len(output) >= 2:
-        extra_entry = output[0].copy()
-        extra_entry['date of birth'] = ''
-        output.append(extra_entry)
     
     return output
 
 # Test the function
 input_data = [
     {
-        "name": 'Joshua Nishanth , Also known as Josh, born on 19 july",
+        "name": "Joshua Nishanth , Also known as Josh, born on 19 july",
         "aka": "",
         "date of birth:": "",
         "part": "",
         "other information": "",
-        "name_ner": 'Joshua Nishanth['NAME'] Also known as Josh ['Also Known As'] born on 19 july['Date of Birth']'
+        "name_ner": "Joshua Nishanth['NAME'] Also known as Josh ['ALSO_KNOWN_AS'] born on 19 july['DATE_OF_BIRTH']"
     },
     {
-        "name": 'Joshua Nishanth , Also known as Josh, born on 19 july",
+        "name": "Joshua Nishanth , Also known as Josh, born on 19 july",
         "aka": "",
         "date of birth:": "",
         "part": "",
         "other information": "",
-        "name_ner": 'Joshua Nishanth['NAME'] born on 19 july['Date of Birth']'
+        "name_ner": "Joshua Nishanth['NAME'] born on 19 july['DATE_OF_BIRTH']"
     },
 ]
 
